ddcd70bdf3058020b555f9e6205d94c400c93daa
leei006 <leei006@newschool.edu>
2025-05-05 15:35:19 -0400

    trying to push this code

--- index.js
+++
@@ -1,6 +0,0 @@
-const scrollContainer = document.getElementById('horizontalScroll');
-
-scrollContainer.addEventListener('wheel', function(e) {
-  e.preventDefault();
-  scrollContainer.scrollLeft += e.deltaY;
-});
